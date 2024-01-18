6481df0ab39ae81de49c58077f02d524157c11af
Shariff <zshariff@deloitte.com.au>
2024-01-18 16:54:12 +0800

    Correct font on header and add media queries

--- space-tourism-website-main/space-tourism-website-day2/src/Components/Header/Header.js
+++ space-tourism-website-main/space-tourism-website-day2/src/Components/Header/Header.js
@@ -4,9 +4,9 @@ import './Header.scss'
 
 const Header = () => {
     return (
-        <header className='primary-navigation-bar text-white'>
+        <header className='flex primary-navigation-bar text-white uppercase ff-sans-cond letter-spacing-2'>
             <img alt='Space Tourism Logo' src={logo}/>
-            <ul className='tabs-list'>
+            <ul className='flex tabs-list'>
                 <li><a>00 Home</a></li>
                 <li><a>01 Destination</a></li>
                 <li><a>02 Crew</a></li>
